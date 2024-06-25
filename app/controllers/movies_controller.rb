3764eaa94137870eba216504e06776a87879eef2
jeremybaxter <jrbaxter94@gmail.com>
2024-06-25 11:54:14 -0500

    Done

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,6 @@
 class MoviesController < ApplicationController
   def new
     @the_movie = Movie.new
-
-    render template: "movies/new"
   end
 
   def index
@@ -16,7 +14,6 @@ class MoviesController < ApplicationController
       end
 
       format.html do
-        render({ :template => "movies/index" })
       end
     end
   end
@@ -27,8 +24,6 @@ class MoviesController < ApplicationController
     matching_movies = Movie.where({ :id => the_id })
 
     @the_movie = matching_movies.first
-
-    render({ :template => "movies/show" })
   end
 
   def create
@@ -38,7 +33,7 @@ class MoviesController < ApplicationController
 
     if @the_movie.valid?
       @the_movie.save
-      redirect_to("/movies", { :notice => "Movie created successfully." })
+      redirect_to(movies_url, { :notice => "Movie created successfully." })
     else
       render template: "movies/new"
     end
@@ -50,8 +45,6 @@ class MoviesController < ApplicationController
     matching_movies = Movie.where({ :id => the_id })
 
     @the_movie = matching_movies.first
-
-    render({ :template => "movies/edit" })
   end
 
   def update
@@ -63,9 +56,9 @@ class MoviesController < ApplicationController
 
     if the_movie.valid?
       the_movie.save
-      redirect_to("/movies/#{the_movie.id}", { :notice => "Movie updated successfully."} )
+      redirect_to(movie_url, { :notice => "Movie updated successfully." })
     else
-      redirect_to("/movies/#{the_movie.id}", { :alert => "Movie failed to update successfully." })
+      redirect_to(edit_movie_url, { :alert => "Movie failed to update successfully." })
     end
   end
 
@@ -75,6 +68,6 @@ class MoviesController < ApplicationController
 
     the_movie.destroy
 
-    redirect_to("/movies", { :notice => "Movie deleted successfully."} )
+    redirect_to(movies_url, { :notice => "Movie deleted successfully."} )
   end
 end
